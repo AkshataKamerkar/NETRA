aa3055c1b30e921d3ab20309b313096cff6525db
AK <akshatakamerkar4@gmail.com>
2024-07-21 20:40:29 +0530

    updated

--- NETRA/settings.py
+++ NETRA/settings.py
@@ -193,6 +193,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'datasciencetestmail@gmail.com'
-EMAIL_HOST_PASSWORD = 'ckya zcpe kraf clss'
+EMAIL_HOST_PASSWORD = os.environ(Pass)
 
 
